273d04f75a44d37042066d36ee829cb678ffbc56
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-07-30 13:45:40 -0300

    feat: add user repository

---
+++ user-service-api/src/main/java/io/github/eduardoconceicao90/user_service_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package io.github.eduardoconceicao90.user_service_api.repository;
+
+import io.github.eduardoconceicao90.user_service_api.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
