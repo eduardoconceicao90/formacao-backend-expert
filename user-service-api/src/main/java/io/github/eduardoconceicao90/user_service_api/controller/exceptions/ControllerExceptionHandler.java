dda9205353443ba49ea318f80259f7fe18893c13
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-08-01 13:59:42 -0300

    feat: add exception handler for object not found

---
+++ user-service-api/src/main/java/io/github/eduardoconceicao90/user_service_api/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package io.github.eduardoconceicao90.user_service_api.controller.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import models.exceptions.ResourceNotFoundException;
+import models.exceptions.StandardError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    ResponseEntity<StandardError> handleNotFoundException(
+            final ResourceNotFoundException ex, final HttpServletRequest request
+            ){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                StandardError.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .path(request.getRequestURI())
+                    .build()
+        );
+    }
+
+}
--- user-service-api/src/main/java/io/github/eduardoconceicao90/user_service_api/service/UserService.java
+++ user-service-api/src/main/java/io/github/eduardoconceicao90/user_service_api/service/UserService.java
@@ -1,9 +1,9 @@
 package io.github.eduardoconceicao90.user_service_api.service;
 
-import io.github.eduardoconceicao90.user_service_api.entity.User;
 import io.github.eduardoconceicao90.user_service_api.mapper.UserMapper;
 import io.github.eduardoconceicao90.user_service_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +15,10 @@ public class UserService {
     private final UserMapper userMapper;
 
     public UserResponse findById(final String id){
-        return userMapper.fromEntity(userRepository.findById(id).orElse(null));
+        return userMapper.fromEntity(userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName()
+                ))
+        );
     }
 }
