616dda710fad04719b9545014cf4898c2af75c05
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-01-28 15:49:34 -0300

    test: add integration test to find by id user controller

---
+++ user-service-api/src/test/java/io/github/eduardoconceicao90/user_service_api/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,44 @@
+package io.github.eduardoconceicao90.user_service_api.controller.impl;
+
+import io.github.eduardoconceicao90.user_service_api.entity.User;
+import io.github.eduardoconceicao90.user_service_api.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static io.github.eduardoconceicao90.user_service_api.creator.CreatorUtils.generateMock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testFindByIdWithSuccess() throws Exception {
+        final var entity = generateMock(User.class);
+        final var userId = userRepository.save(entity).getId();
+
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.name").value(entity.getName()))
+                .andExpect(jsonPath("$.email").value(entity.getEmail()))
+                .andExpect(jsonPath("$.password").value(entity.getPassword()))
+                .andExpect(jsonPath("$.profiles").isArray());
+
+        userRepository.deleteById(userId);
+    }
+
+}
