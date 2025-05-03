f808ba9072e99c901eb2b985f25fc7c0a04908e1
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-05-03 14:37:28 -0300

    chore: add github access

--- config-server/src/main/java/io/github/eduardoconceicao90/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/io/github/eduardoconceicao90/config_server/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package io.github.eduardoconceicao90.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
