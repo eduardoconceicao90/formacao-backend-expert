41dc711c89522790280023c15a2012198a5593f6
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-05-10 07:39:00 -0300

    fix: bugfix on time zone

--- order-service-api/src/main/java/io/github/eduardoconceicao90/order_service_api/entity/Order.java
+++ order-service-api/src/main/java/io/github/eduardoconceicao90/order_service_api/entity/Order.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import models.enums.OrderStatusEnum;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serial;
@@ -49,6 +50,7 @@ public class Order implements Serializable {
     @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime closedAt;
 
 }
