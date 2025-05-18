80dd9217210a771c86ffd70ca9d5effdd34538db
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-05-18 13:09:50 -0300

    feat: add api gateway

---
+++ api-gateway/src/main/java/io/github/eduardoconceicao90/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package io.github.eduardoconceicao90.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+					.routes()
+						.route(r -> r.path("/api/users/**").uri("lb://user-service-api"))
+					.build();
+	}
+
+}
