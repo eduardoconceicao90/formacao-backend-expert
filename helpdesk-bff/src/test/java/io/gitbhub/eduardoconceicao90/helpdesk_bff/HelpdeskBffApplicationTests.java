baa0aa4a2f7525ce0b8009826ed6fd09da9911ac
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-05-21 19:54:50 -0300

    chore: start helpdesk bff project

---
+++ helpdesk-bff/src/test/java/io/gitbhub/eduardoconceicao90/helpdesk_bff/HelpdeskBffApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitbhub.eduardoconceicao90.helpdesk_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
