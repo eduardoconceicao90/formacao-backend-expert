f3bfb5f764701e16cac017489d349f6a1fed9a0f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-05-25 15:53:50 -0300

    feat: add security public matchers

--- helpdesk-bff/src/main/java/io/gitbhub/eduardoconceicao90/helpdesk_bff/config/SecurityConfig.java
+++ helpdesk-bff/src/main/java/io/gitbhub/eduardoconceicao90/helpdesk_bff/config/SecurityConfig.java
@@ -1,15 +1,18 @@
-package io.github.eduardoconceicao90.auth_service_api.config;
+package io.gitbhub.eduardoconceicao90.helpdesk_bff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     public static final String[] SWAGGER_WHITELIST = {
@@ -34,8 +37,4 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
